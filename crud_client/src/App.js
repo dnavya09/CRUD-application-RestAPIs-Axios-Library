c068cbf93bd87a40f8fe1f21065198c27e7d4a9f
dnavya09 <123893334+dnavya09@users.noreply.github.com>
2023-11-15 00:08:35 +0530

    Add files via upload

---
+++ crud_client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Home from './Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Create from './Create'
+import Read from './Read'
+import Update from './Update';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/create' element={<Create />}/>
+        <Route path='/read/:id' element={<Read />}/>
+        <Route path='/edit/:id' element={<Update />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ crud_client/src/Update.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import { useNavigate, useParams} from 'react-router-dom';
+
+function Update() {
+  const {id} = useParams();
+  const navigate = useNavigate();
+  useEffect(() =>{
+    axios.get('http://localhost:8081/read/'+id)
+    .then(res => {
+        console.log(res)
+        setValues({
+          ...values,
+          name: res.data[0].Name,
+          email: res.data[0].Email
+        });
+    })
+    .catch(err => console.log(err))
+    },[])
+
+    const [values, setValues] = useState({
+        name:'',
+        email:''
+        })  
+
+    const handleUpdate=(eve) => {
+      eve.preventDefault();
+      axios.put('http://localhost:8081/update/'+id, values)
+      .then(res => {
+        console.log(res)
+        navigate('/')
+       })
+       .catch(err => console.log(err))
+    }
+       
+
+  return (
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+        <div className="w-50 bg-white rounded p-3">
+        <form onSubmit={handleUpdate}>
+            <h2>Add Student</h2>
+            <div className='mb-2'>
+                <label htmlFor="">Name</label>
+                <input type='text' placeholder='Enter Name' className='form-control' value={values.name}
+                 onChange={e => setValues({...values, name:e.target.value})}
+                />
+            </div>
+            <div className='mb-2'>
+                <label htmlFor="">Email</label>
+                <input type='email' placeholder='Enter Email' className='form-control' value={values.email}
+                onChange={e => setValues({...values, email:e.target.value})}
+                />
+            </div>
+            <button className='btn btn-success'>Submit</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Update
